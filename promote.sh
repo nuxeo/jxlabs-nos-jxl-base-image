46ac93d9be25f804eb4edde7302ab0e88990158e
Stephane Lacoin (aka nxmatic) <slacoin@nuxeo.com>
2020-10-22 15:33:56 +0200

    promote also jxlabs-nos-jxl

--- promote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "promoting the new version ${VERSION} to downstream repositories"
-
-jx step create pr regex --regex "(?m)^FROM gcr.io/build-jx-prod/jxlabs-nos-master/jxlabs-nos-jxl-base-image:(?P<version>.*)$" --version ${VERSION} --files Dockerfile --repo https://github.com/nuxeo/jxlabs-nos-jxl.git
